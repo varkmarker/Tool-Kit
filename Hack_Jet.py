8584ee5c7f461e04bce8643857002839bc1b12e5
varkmarker <varkmarker@gmail.com>
2023-04-13 22:18:20 +0530

    Kali Repo Code Added,it is put in command

--- Hack_Jet.py
+++ Hack_Jet.py
@@ -4,3 +4,25 @@ from colr import Colr as Color
 
 banner = pyfiglet.figlet_format(" HACK JET")
 print(Color().hex("#ff0000", banner, rgb_mode=True))
+
+# with open("/etc/apt/sources.list", "a") as file:
+#     file.write(
+#         "\n".join(
+#             [
+#                 "#KALI LINUX REPOSITORY ",
+#                 "deb http://http.kali.org/kali kali-rolling main contrib non-free",
+#             ]
+#         )
+#     )
+
+
+# replace = ["#deb http://http.kali.org/kali kali-rolling main contrib non-free"]
+
+# with open("/etc/apt/sources.list", "r") as file:
+#     Commandrepo = file.read()
+
+#     Commandrepo = Commandrepo.replace(
+#         "deb http://http.kali.org/kali kali-rolling main contrib non-free", replace[0]
+#     )
+#     with open("/etc/apt/sources.list", "w") as file:
+#         file.write(""+Commandrepo)
