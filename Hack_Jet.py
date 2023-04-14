e9d0cc91a68833c2c63b92512ff554cbaa74070e
varkmarker <varkmarker@gmail.com>
2023-04-14 16:00:51 +0530

    Repo add function completed and new banner add for the end of the script

--- Hack_Jet.py
+++ Hack_Jet.py
@@ -1,28 +1,80 @@
 import pyfiglet
+import os
 from colr import Colr as Color
 
-
+# Main Banner
 banner = pyfiglet.figlet_format(" HACK JET")
 print(Color().hex("#ff0000", banner, rgb_mode=True))
 
-# with open("/etc/apt/sources.list", "a") as file:
-#     file.write(
-#         "\n".join(
-#             [
-#                 "#KALI LINUX REPOSITORY ",
-#                 "deb http://http.kali.org/kali kali-rolling main contrib non-free",
-#             ]
-#         )
-#     )
+
+# End Banner
+def the_end():
+    banner = pyfiglet.figlet_format(" THE END")
+    print(Color().hex("#ff0000", banner, rgb_mode=True))
 
 
-# replace = ["#deb http://http.kali.org/kali kali-rolling main contrib non-free"]
+# Add Repository
+def add_repo():
+    with open("/etc/apt/sources.list", "a") as file:
+        file.write(
+            "\n".join(
+                [
+                    "#KALI LINUX REPOSITORY ",
+                    "deb http://http.kali.org/kali kali-rolling main contrib non-free",
+                ]
+            )
+        )
 
-# with open("/etc/apt/sources.list", "r") as file:
-#     Commandrepo = file.read()
+    os.system(
+        "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys ED444FF07D8D0BF6",
+    )
 
-#     Commandrepo = Commandrepo.replace(
-#         "deb http://http.kali.org/kali kali-rolling main contrib non-free", replace[0]
-#     )
-#     with open("/etc/apt/sources.list", "w") as file:
-#         file.write(""+Commandrepo)
+    os.system("sudo apt-get update")
+
+
+add_repo()
+
+
+# Comment Repository
+def comment_repo():
+    replace = ["#deb http://http.kali.org/kali kali-rolling main contrib non-free"]
+
+    with open("/etc/apt/sources.list", "r") as file:
+        Commandrepo = file.read()
+
+        Commandrepo = Commandrepo.replace(
+            "deb http://http.kali.org/kali kali-rolling main contrib non-free",
+            replace[0],
+        )
+    with open("/etc/apt/sources.list", "w") as file:
+        file.write("" + Commandrepo)
+
+
+def case_default():
+    print("Invalid option")
+    banner = pyfiglet.figlet_format("Kali Repo Not Comment Yet")
+    print(Color().hex("#ff0000", banner, rgb_mode=True))
+
+
+answer = input(
+    Color().hex(
+        "#af50a2",
+        "If you want to comment [ # ] the repository line of kali linux in the source file that adds by this script y or n : ",
+        rgb_mode=True,
+    )
+)
+
+# Switch Directory
+switch = {
+    "y": comment_repo,
+    "Y": comment_repo,
+    "yes": comment_repo,
+    "YES": comment_repo,
+    "NO": the_end,
+    "n": the_end,
+    "no": the_end,
+    "N": the_end,
+}
+choice = answer
+switch_case = switch.get(choice, case_default)
+switch_case()
